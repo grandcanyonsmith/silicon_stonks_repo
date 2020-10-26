9bcbdcbf6db9cb4f19457419d4a07b1cf1016b13
Austin <phillips.austin51@gmail.com>
2020-10-26 13:05:27 -0600

    fix server issue

--- server/app.js
+++ server/app.js
@@ -19,39 +19,39 @@ const dev = app.get('env') !== 'production';
 const PORT = process.env.PORT || 5000;
 const db = mongoose.connection;
 
-app.use(cookieparser())
-app.use(bodyParser.json());
-app.use(helmet());
-app.use(cors());
-app.use(morgan('common'));
-app.use((err, req, res, next) => {
-  res.json(err);
-});
-
-// Handle Sessions
-app.use(session({
-  secret: process.env.sessionSecret,
-  saveUninitialized: true,
-  resave: true
-}));
-
-// Passport
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use('/api', routes);
-
-if (!dev) {
-  app.use(express.static(path.resolve(__dirname, '../build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
-  });
-};
-
-// app.use(express.static(path.join(__dirname, "../build")));
-
-// app.get("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "../build", "index.html"));
+// app.use(cookieparser())
+// app.use(bodyParser.json());
+// app.use(helmet());
+// app.use(cors());
+// app.use(morgan('common'));
+// app.use((err, req, res, next) => {
+//   res.json(err);
 // });
+
+// // Handle Sessions
+// app.use(session({
+//   secret: process.env.sessionSecret,
+//   saveUninitialized: true,
+//   resave: true
+// }));
+
+// // Passport
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// app.use('/api', routes);
+
+// if (!dev) {
+//   app.use(express.static(path.resolve(__dirname, '../build')));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+//   });
+// };
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
