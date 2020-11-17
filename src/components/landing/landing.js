6c79a2633c72b95a33db6c4fa4ba22e19c48e668
grandcanyonsmith <canyon@bottega.tech>
2020-11-17 15:47:30 -0700

    asdf

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -32,6 +32,11 @@ function Landing() {
             </div>
           </div>
           <ul className='landing__stock-list'>
+          <div className="percent-change-title">
+            <div className="percent-change-column-1"></div>
+            <div className="percent-change-column-2">% change since<br></br>
+            Nov. 1st recommendation</div>
+          </div>
               {STONKS.map(stonk => {
                   return (
                     <div key={stonk.key}>
